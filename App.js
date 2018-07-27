053058cc8be27f6e06cb39f98758a355c76a8333
Michael Dorsey <mikedorsey1021@gmail.com>
2018-07-27 17:25:02 -0500

    starting point with login and registration page

--- App.js
+++ App.js
@@ -1,31 +1,114 @@
-import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
+import { Container, Header, Content, Form, Item, Input, Button } from 'native-base';
 
-export default class LotsOfStyles extends Component {
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View>
-        <Text style={[styles.red, styles.center]}>just red</Text>
-        <Text style={[styles.bigblue, styles.center]}>just bigblue</Text>
-        <Text style={[styles.bigblue, styles.red, styles.center]}>bigblue, then red</Text>
-        <Text style={[styles.red, styles.bigblue, styles.center]}>red, then bigblue</Text>
+
+
+     
+      <Container>
+        <Header />
+        <Content>
+          <Form>
+            <Item>
+              <Input placeholder="Email" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+          </Form>
+        <Button primary title="Go to Details" onPress={() => this.props.navigation.navigate('Details')}><Text> Sign in </Text></Button>
+        <Button primary title="Go to Details" onPress={() => this.props.navigation.navigate('Register')}><Text> Register </Text></Button>
+
+        </Content>
+      </Container>
+      
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button onPress={() => this.props.navigation.push('Details')}><Text> Go to Details again </Text></Button>
+        <Button onPress={() => this.props.navigation.push('Home')}><Text> Home </Text></Button>
+        <Button onPress={() => this.props.navigation.goBack()}><Text> Go Back </Text></Button>
+
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  center: {
-    textAlign: 'center',
-    marginTop: 50
-  },
+class RegisterScreen extends React.Component {
 
-  bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
+  render() {
+    return(
+
+      <Container>
+        <Header />
+        <Content>
+          <Form>
+            <Item>
+              <Input placeholder="First Name" />
+            </Item>
+            <Item >
+              <Input placeholder="Last Name" />
+            </Item>
+            <Item >
+              <Input placeholder="Email" />
+            </Item>
+            <Item >
+              <Input placeholder="Birthdate" />
+            </Item>
+            <Item >
+              <Input placeholder="Password" />
+            </Item>
+            <Item last>
+              <Input placeholder="Confirm password" />
+            </Item>
+          </Form>
+        <Button primary title="Go to Details" onPress={() => this.props.navigation.navigate('Details')}><Text> Submit </Text></Button>
+        
+
+        </Content>
+      </Container>
+      );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+
+    Register: {
+      screen: RegisterScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
   },
-  red: {
-    color: 'red',
-  },
-});
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
