491e6b781924e896d92fa092db89fe0e67461d9c
Michael Dorsey <mikedorsey1021@gmail.com>
2018-07-28 12:21:01 -0500

    added some cards to the home screen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, Linking, ScrollView } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation'; // Version can be specified in package.json
 import { Container, Header, Content, Form, Item, Input, Button, Card, CardItem, Body } from 'native-base';
 // import FontAwesome, { Icons } from 'react-native-fontawesome';
@@ -32,16 +32,50 @@ class SignIn extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
+  
+
   render() {
+    const images = [
+  {
+    key: 1,
+    name: "Nathan Anderson",
+    image: require("./images/1.jpg"),
+    url: "https://unsplash.com/photos/C9t94JC4_L8"
+  },
+  {
+    key: 2,
+    name: "Jamison McAndie",
+    image: require("./images/2.jpg"),
+    url: "https://unsplash.com/photos/waZEHLRP98s"
+  },
+  {
+    key: 3,
+    name: "Alberto Restifo",
+    image: require("./images/3.jpg"),
+    url: "https://unsplash.com/photos/cFplR9ZGnAk"
+  },
+  {
+    key: 4,
+    name: "John Towner",
+    image: require("./images/4.jpg"),
+    url: "https://unsplash.com/photos/89PFnHKg8HE"
+  }
+];
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
+      <View style={{ flex: 1 }}>
+    <ScrollView contentContainerStyle={{ paddingVertical: 20 }}>
+      {images.map(({ name, image, url, key }) => (
+        <Card title={`CARD ${key}`} image={image} key={key}>
+          <Text style={{ marginBottom: 10 }}>
+            Photo by {name}.
+          </Text>
         <Button onPress={() => this.props.navigation.push('Details')}><Text> Go to Details again </Text></Button>
         <Button onPress={() => this.props.navigation.push('Home')}><Text> Home </Text></Button>
         <Button onPress={() => this.props.navigation.goBack()}><Text> Go Back </Text></Button>
-
-        
-      </View>
+        </Card>
+      ))}
+    </ScrollView>
+  </View>
     );
   }
 }
@@ -117,7 +151,7 @@ class ProfileScreen extends React.Component {
   }
 }
 
-const RootStack = createStackNavigator({
+const SignedOut = createStackNavigator({
     SignUp: {
     screen: RegisterScreen,
     navigationOptions: {
@@ -130,13 +164,6 @@ const RootStack = createStackNavigator({
       title: "Sign In"
     }
   },
-
-  Home: {
-    screen: DetailsScreen,
-    navigationOptions: {
-      title: "Details Page"
-    }
-  }
 });
 
 export const SignedIn = createBottomTabNavigator({
