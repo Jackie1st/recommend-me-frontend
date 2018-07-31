5b833a539d4791cf4a7b350eaecbb50343aa72a5
Michael Dorsey <mikedorsey1021@gmail.com>
2018-07-31 17:32:59 -0500

    able to login and create a user

--- app/testFetch.js
+++ app/testFetch.js
@@ -1,18 +1,40 @@
 var fetch = require('node-fetch');
 
-var url = 'https://reccme.herokuapp.com/oauth/token';
-var data = {"email": "testboy@test.com", "password": "testtest", "grant_type": "password"};
-const fetchData = (articleID) =>{
-return fetch(url, {
+// var url = 'https://reccme.herokuapp.com/oauth/token';
+// var data = {"email": "testboy@test.com", "password": "testtest", "grant_type": "password"};
+// const fetchData = (articleID) =>{
+// return fetch(url, {
+//   method: 'POST', // or 'PUT'
+//   body: JSON.stringify(data), // data can be `string` or {object}!
+//   headers:{
+//     'Content-Type': 'application/json'
+//   }
+// }).then(res => {
+//   res.json().then(response_json => console.log(response_json.access_token));
+// })
+// // .then(token => console.log(token.access_token))
+// }
+
+
+const url2 = 'https://reccme.herokuapp.com/users';
+const userData = {"user": {"first_name": 'mike',
+"last_name": 'dorsey',
+"birthdate": '21/10/1988',
+"email": "test@me.com",
+"password": 'test123'}}
+
+const createUser = (articleID) =>{
+return fetch(url2, {
   method: 'POST', // or 'PUT'
-  body: JSON.stringify(data), // data can be `string` or {object}!
+  body: JSON.stringify(userData), // data can be `string` or {object}!
   headers:{
     'Content-Type': 'application/json'
   }
 }).then(res => {
-  res.json().then(response_json => console.log(response_json.access_token));
+  res.json().then(response_json => console.log(response_json));
 })
 // .then(token => console.log(token.access_token))
 }
 
-fetchData(); 
+
+createUser(); 
